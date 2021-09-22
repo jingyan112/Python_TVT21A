fb6f90ba6ea95045523625ba820559312c7a14d0
JING YAN <jingyan27@outlook.com>
2021-09-22 15:51:44 +0300

    Update osa08_all_script.py

--- osa08_all_script.py
+++ osa08_all_script.py
@@ -37,3 +37,61 @@ if __name__ == "__main__":
     rivien_summat(matriisi)
     print(matriisi)
 
+#osa8-03 tee ratkaisu tänne
+# Muista import-lause:
+"""
+Make a function vuodet_listaan(paivamaarat: list) that gets a list date of objects of type of type as its parameter.
+The function returns a new list with the years of the dates in order of magnitude from smallest to largest.
+"""
+from datetime import date
+
+def vuodet_listaan(paivamaarat: list):
+    new_list = []
+    for item in paivamaarat:
+        new_list.append(item.year)
+    return sorted(new_list)
+
+if __name__ == "__main__":
+    paiva1 = date(2019, 2, 3)
+    paiva2 = date(2006, 10, 10)
+    paiva3 = date(1993, 5, 9)
+    vuodet = vuodet_listaan([paiva1, paiva2, paiva3])
+    print(vuodet)
+
+#osa8-04 ÄLÄ MUUTA ALLA OLEVAA LUOKKAA Kauppalista!
+# Kirjoita ratkaisusi luokan alapuolelle!
+"""
+Using the examples, make a function tuotteita_yhteensa(lista: Kauppalista) that gets an Kauppalistaobject of type parameter.
+The function calculates the total number of products in the list and returns it.
+"""
+class Kauppalista:
+    def __init__(self):
+        self.tuotteet = []
+
+    def tuotteita(self):
+        return len(self.tuotteet)
+
+    def lisaa(self, tuote: str, maara: int):
+        self.tuotteet.append((tuote, maara))
+
+    def tuote(self, n: int):
+        return self.tuotteet[n - 1][0]
+
+    def maara(self, n: int):
+        return self.tuotteet[n - 1][1]
+
+# ----------------------
+# Tee ratkaisusi tähän:
+# ----------------------
+def tuotteita_yhteensa(lista: Kauppalista):
+    total_num = 0
+    for index in range(0, lista.tuotteita()):
+        total_num = total_num + lista.maara(index)
+    return total_num
+
+if __name__ == "__main__":
+    lista = Kauppalista()
+    lista.lisaa("banaanit", 10)
+    lista.lisaa("omenat", 5)
+    lista.lisaa("ananas", 1)
+    print(tuotteita_yhteensa(lista))
