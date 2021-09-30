726ab6d82b4ff13bb37cc5157d6cf510a6243a68
JING YAN <jingyan27@outlook.com>
2021-09-30 20:24:24 +0300

    Update osa09_all_script.py

--- osa09_all_script.py
+++ osa09_all_script.py
@@ -616,3 +616,25 @@ if __name__ == "__main__":
     print(tili.saldo)   # 891.0
     tili.talleta(100)
     print(tili.saldo)   # 981.09
+
+#osa9-13
+"""
+Add a category variable - dictionary type
+"""
+class Kaupunki:
+    postinumerot = {"Helsinki": "00100", "Turku": "20100", "Tampere": "33100", "Jyväskylä": "40100", "Oulu": "90100"}
+    
+    def __init__(self, nimi: str, asukasluku: int):
+        self.__nimi = nimi
+        self.__asukasluku = asukasluku
+
+    @property
+    def nimi(self):
+        return self.__nimi
+
+    @property
+    def asukasluku(self):
+        return self.__asukasluku
+
+    def __str__(self):
+        return f"{self.__nimi} ({self.__asukasluku} as.)"
