c41aa8eeccd9bc1920da61dfee25b487447101e7
JING YAN <jingyan27@outlook.com>
2021-10-06 14:40:34 +0200

    Update osa11_all_script.py

--- osa11_all_script.py
+++ osa11_all_script.py
@@ -48,3 +48,34 @@ if __name__ == "__main__":
     rivit = tahtirivit([4, 3, 2, 1, 10])
     for rivi in rivit:
         print(rivi)
+
+#osa11-3
+"""
+Using a list compilation to implement parhaat_tulokset(suoritukset: list)
+gets a list of test execution objects as its parameter.
+returns a new list that stores the best rating for each run.
+
+Output:
+[5, 4, 3]
+"""
+# class Koesuoritus: name, rating1, rating2, rating3
+class Koesuoritus:
+    def __init__(self, nimi: str, arvosana1: int, arvosana2: int, arvosana3: int):
+        self.nimi = nimi
+        self.arvosana1 = arvosana1
+        self.arvosana2 = arvosana2
+        self.arvosana3 = arvosana3
+
+    def __str__(self):
+        return (f'Nimi:{self.nimi}, arvosana1: {self.arvosana1}' +
+            f', arvosana2: {self.arvosana2}, arvosana3: {self.arvosana3}')
+
+def parhaat_tulokset(suoritukset: list):
+    return [max([element.arvosana1, element.arvosana2, element.arvosana3]) for element in suoritukset]
+
+if __name__ == "__main__":
+    suoritus1 = Koesuoritus("Pekka",5,3,4)
+    suoritus2 = Koesuoritus("Pirjo",3,4,1)
+    suoritus3 = Koesuoritus("Paavo",2,1,3)
+    suoritukset = [suoritus1, suoritus2, suoritus3]
+    print(parhaat_tulokset(suoritukset))
