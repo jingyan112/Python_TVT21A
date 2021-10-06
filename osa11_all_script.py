6340c825b7a1413b7972b642e7ee738cb183a647
JING YAN <jingyan27@outlook.com>
2021-10-06 15:53:36 +0200

    Update osa11_all_script.py

--- osa11_all_script.py
+++ osa11_all_script.py
@@ -220,3 +220,55 @@ if __name__ == "__main__":
     print("kauppalistalla vähintään 8 seuraavia tuotteita:")
     for tuote in kauppalistan_tuotteet(lista, 8):
         print(tuote)
+    
+#osa11-10
+"""
+Using a list compilation with a conditional part to implement kauppalistan_tuotteet(kauppalista: list, maara: int)
+gets a list of dwellings and an individual dwelling to be compared
+returns a list of the apartments that are cheaper than the comparable apartment, and the price difference between them
+
+Output:
+asuntoa Jakomäki kolmio halvemmat vaihtoehdot:
+Eira yksiö                     hintaero 107000 euroa
+Kallio kaksio                  hintaero 35400 euroa
+Suomussalmi omakotitalo        hintaero 87500 euroa
+Kerava 4h ja keittiö           hintaero 16500 euroa
+"""
+class Asunto:
+    def __init__(self, huoneita: int, nelioita: int, neliohinta:int, kuvaus: str):
+        self.huoneita = huoneita
+        self.nelioita = nelioita
+        self.neliohinta = neliohinta
+        self.kuvaus = kuvaus
+
+    def suurempi(self, verrattava):
+        return self.nelioita > verrattava.nelioita
+
+    def hintaero(self, verrattava):
+        ero = abs((self.neliohinta * self.nelioita) - (verrattava.neliohinta * verrattava.nelioita))
+        return ero
+
+    def kalliimpi(self, verrattava):
+        ero = (self.neliohinta * self.nelioita) - (verrattava.neliohinta * verrattava.nelioita)
+        return ero > 0
+
+    def __repr__(self):
+        return (f'Asunto(huoneita = {self.huoneita}, nelioita = {self.nelioita}, ' + 
+            f'neliohinta = {self.neliohinta}, kuvaus = {self.kuvaus})')
+
+def halvemmat(asunnot: list, verrattava: Asunto):
+    return [(item, item.hintaero(verrattava)) for item in asunnot if not item.kalliimpi(verrattava) and item != verrattava]
+
+if __name__ == "__main__":
+    a1 = Asunto(1, 16, 5500, "Eira yksiö")
+    a2 = Asunto(2, 38, 4200, "Kallio kaksio")
+    a3 = Asunto(3, 78, 2500, "Jakomäki kolmio")
+    a4 = Asunto(6, 215, 500, "Suomussalmi omakotitalo")
+    a5 = Asunto(4, 105, 1700, "Kerava 4h ja keittiö")
+    a6 = Asunto(25, 1200, 2500, "Haikon kartano")
+
+    asunnot = [a1, a2, a3, a4, a5, a6]
+
+    print(f"asuntoa {a3.kuvaus} halvemmat vaihtoehdot:")
+    for alkio in halvemmat(asunnot, a3):
+        print(f"{alkio[0].kuvaus:30} hintaero {alkio[1]} euroa")
