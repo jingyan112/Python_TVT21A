75a1423e0970e19b3868ae52611ccc2fc1818633
JING YAN <jingyan27@outlook.com>
2021-10-08 09:51:56 +0200

    Update osa12_all_script.py

--- osa12_all_script.py
+++ osa12_all_script.py
@@ -113,3 +113,82 @@ if __name__ == "__main__":
     print()
     for reitti in vaikeuden_mukaan(reitit):
         print(reitti)
+
+#osa12-5
+"""
+Given the following 2 class describing the climbing route: name of the route, length, difficulty level
+Implement a function reittien_maaran_mukaan that sort the climbing cliffs in ascending order according to the number of routes.
+Implement a function vaikeimman_reitin_mukaan that sort the climbing cliffs in descending order of the difficulty level.
+
+Output:
+Nummi 1 reittiä, vaikein 8C+
+Olhava 3 reittiä, vaikein 6B
+Nalkkilan släbi 4 reittiä, vaikein 7A
+
+Nummi 1 reittiä, vaikein 8C+
+Nalkkilan släbi 4 reittiä, vaikein 7A
+Olhava 3 reittiä, vaikein 6B
+"""
+class Kiipeilyreitti:
+    def __init__(self, nimi: str, pituus: int, grade: str):
+        self.nimi = nimi
+        self.pituus = pituus
+        self.grade = grade
+
+    def __str__(self):
+        return f"{self.nimi}, pituus {self.pituus} metriä, grade {self.grade}"
+
+class Kiipeilykallio:
+    def __init__(self, nimi: str):
+        self.nimi = nimi
+        self.__reitit = []
+
+    def lisaa_reitti(self, reitti: Kiipeilyreitti):
+        self.__reitit.append(reitti)
+
+    def reitteja(self):
+        return len(self.__reitit)
+
+    def vaikein_reitti(self):
+        def vaikeuden_mukaan(reitti):
+            return reitti.grade
+        reitit_jarjestyksessa = sorted(self.__reitit, key=vaikeuden_mukaan)
+        return reitit_jarjestyksessa[-1]
+
+    def __str__(self):
+        vaikein_reitti = self.vaikein_reitti()
+        return f"{self.nimi} {self.reitteja()} reittiä, vaikein {vaikein_reitti.grade}"
+
+def reittien_maaran_mukaan(kalliot: list):
+    def class_item(class_item: Kiipeilykallio):
+        return class_item.reitteja()
+    return sorted(kalliot, key = class_item)
+
+def vaikeimman_reitin_mukaan(kalliot: list):
+    def class_item(class_item: Kiipeilykallio):
+        return class_item.vaikein_reitti().grade
+    return sorted(kalliot, key = class_item, reverse = True)
+
+if __name__ == "__main__":
+    k1 = Kiipeilykallio("Olhava")
+    k1.lisaa_reitti(Kiipeilyreitti("Kantti", 38, "6A+"))
+    k1.lisaa_reitti(Kiipeilyreitti("Suuri leikkaus", 36, "6B"))
+    k1.lisaa_reitti(Kiipeilyreitti("Ruotsalaisten reitti", 42, "5+"))
+
+    k2 = Kiipeilykallio("Nummi")
+    k2.lisaa_reitti(Kiipeilyreitti("Syncro", 14, "8C+"))
+
+    k3 = Kiipeilykallio("Nalkkilan släbi")
+    k3.lisaa_reitti(Kiipeilyreitti("Pieniä askelia", 12, "6A+"))
+    k3.lisaa_reitti(Kiipeilyreitti("Smooth operator", 11, "7A"))
+    k3.lisaa_reitti(Kiipeilyreitti("Possu ei pidä", 12 , "6B+"))
+    k3.lisaa_reitti(Kiipeilyreitti("Hedelmätarha", 8, "6A"))
+
+    kalliot = [k1, k2, k3]
+    for kallio in reittien_maaran_mukaan(kalliot):
+        print(kallio)
+    print()
+
+    for kallio in vaikeimman_reitin_mukaan(kalliot):
+        print(kallio)
+    print()
