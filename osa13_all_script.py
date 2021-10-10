3a4ffb907a82380d48b2347e703a6e1975be8676
JING YAN <jingyan27@outlook.com>
2021-10-10 18:31:57 +0200

    Update osa13_all_script.py

--- osa13_all_script.py
+++ osa13_all_script.py
@@ -94,3 +94,36 @@ while True:
     for tapahtuma in pygame.event.get():
         if tapahtuma.type == pygame.QUIT:
             exit()
+
+#osa13-5
+"""
+Make the robot keep moving up and down, once it reaches the boundry, chgange the direction
+"""
+import pygame
+
+pygame.init()
+naytto = pygame.display.set_mode((640, 480))
+
+robo = pygame.image.load("robo.png")
+
+x = 0
+y = 0
+nopeus = 1
+kello = pygame.time.Clock()
+
+while True:
+    for tapahtuma in pygame.event.get():
+        if tapahtuma.type == pygame.QUIT:
+            exit()
+
+    naytto.fill((0, 0, 0))
+    naytto.blit(robo, (x, y))
+    pygame.display.flip()
+
+    y += nopeus
+    if nopeus > 0 and y + robo.get_height() >= 480:
+        nopeus = -nopeus
+    if nopeus < 0 and y <= 0:
+        nopeus = -nopeus
+
+    kello.tick(60)
